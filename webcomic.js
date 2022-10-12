2ebb1930c42fe21bfdcba8eef7f0e8e8a0fd1ca6
PlushFacts <115669299+PlushFacts@users.noreply.github.com>
2022-10-13 08:46:19 +1100

    first comment :)

---
+++ webcomic.js
@@ -0,0 +1,60 @@
+let pagenumber = 1;
+let pageamount = 93;
+
+function goright() {
+	if (pagenumber < pageamount) {
+	pagenumber += 1;
+	document.getElementById("image").src = "pages/"+pagenumber.toString()+".png";
+	settext();
+	}
+	
+}
+
+function goleft() {
+	if (pagenumber > 1) {
+	pagenumber -= 1;
+	document.getElementById("image").src = "pages/"+pagenumber.toString()+".png";
+	settext();
+	}
+}
+
+function gofirst() {
+	pagenumber = 1;
+	document.getElementById("image").src = "pages/"+pagenumber.toString()+".png";
+	settext();
+}
+
+function golast() {
+	pagenumber = pageamount
+	document.getElementById("image").src = "pages/"+pagenumber.toString()+".png";
+	settext();
+}
+
+function settext() {
+	document.getElementById("pagedate").innerHTML = "Page " + pagenumber;
+}
+
+function getnumber() {
+	if (parseInt(document.getElementById('pagenum').value) <= pageamount) {
+		if (parseInt(document.getElementById('pagenum').value) > 0) {
+			pagenumber = parseInt(document.getElementById('pagenum').value);
+			document.getElementById("image").src = "pages/"+pagenumber.toString()+".png";
+			settext();
+		}
+	}
+}
+
+window.onload = function() {
+	settext();
+};
+
+document.onkeypress = function(evt) {
+    evt = evt || window.event;
+    var charCode = evt.keyCode || evt.which;
+	if (charCode == 97) {
+		goleft();
+	}
+	if (charCode == 100) {
+		goright();
+	}
+};
